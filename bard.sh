ed407a799a8073012d3210cd6b2c47d10db65034
Sergio DellaCioppa <dcsergio@users.noreply.github.com>
2023-09-22 12:02:56 +0200

    Update bard.sh

--- bard.sh
+++ bard.sh
@@ -3,8 +3,11 @@
 delay=100
 
 echo "This is Sergio's Bard"
+sleep $delay
 echo "You can change delay (seconds) between lines"
+sleep $delay
 echo "using input argument. Deafult is $delay"
+sleep $delay
 
 if [[ "$1" != "" ]]; then
   delay=$1
